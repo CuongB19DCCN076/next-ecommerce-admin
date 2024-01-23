dca91e6e3e2ece0b7e983748418d65c961f532b4
Cuong <cuongchamchi1904@gmail.com>
2024-01-23 20:02:22 +0700

    assS

--- src/app/(dashbroad)/[storeId]/(routes)/page.tsx
+++ src/app/(dashbroad)/[storeId]/(routes)/page.tsx
@@ -3,14 +3,14 @@ import prismadb from "@/lib/prismadb"
 const DashbroadPage = async ({ params }: {
     params: { storeId: string }
 }) => {
-    const store = await prismadb.store.findFirst({
+    const stores = await prismadb.store.findFirst({
         where: {
             id: params.storeId
         }
     })
     return (
         <div>
-            Active Store: {store?.name}
+            Active Store: {stores?.name}
         </div>
     )
 }
